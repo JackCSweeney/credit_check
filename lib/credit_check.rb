3544fa73451a5c7688cb32e8b2315419631dcd92
Jack Sweeney <jackcsweeney@gmail.com>
2023-11-27 16:41:08 -0800

    Add code to credit_check.rb to define luhn algorithm

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,7 +1,30 @@
 card_number = "5541808923795240"
 
-# Your Luhn Algorithm Here
+def luhn_algo(number)
+    num_array = number.to_i.digits.reverse
+    doubled_array = []
+    summed_array = []
+    num_array.each_index do |index|
+        if index.even? == true
+            doubled_array << num_array[index] * 2
+        else
+            doubled_array << num_array[index]
+        end
+    end
+    doubled_array.each do |num|
+        if num >= 10
+            summed_array << num.digits.sum
+        else
+            summed_array << num
+        end
+    end
+    total_summed = summed_array.sum
+    if total_summed % 10 == 0
+        "The number #{number} is valid!"
+    else
+        "The number #{number} is invalid!"
+    end
+end
 
-# Output
-## If it is valid, print "The number [card number] is valid!"
-## If it is invalid, print "The number [card number] is invalid!"
+puts luhn_algo("5541808923795240")
+puts luhn_algo(5541801923795240) 
